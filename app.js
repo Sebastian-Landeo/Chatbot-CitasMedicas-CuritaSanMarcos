57459af4e98fcaafc5e899b3a352a1d2464ddd50
Sebastian Landeo <sebastian.landeo@unmsm.edu.pe>
2025-02-06 16:27:05 -0500

    Probando flujos entre flujos

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 const path = require('path')
+const fs = require('fs')
+
+// Usar menu que se encuentre en la carpeta mensajes
+const menuPath = path.join(__dirname, 'mensajes', 'menu.txt')
+const menu = fs.readFileSync(menuPath, 'utf-8')
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
@@ -10,6 +15,15 @@ const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
     .addAnswer('🙌 Hola bienvenido, soy el Curita Bot')
     .addAnswer('Y estoy dispuesto a ayudarte con tus citas en Curita San Marcos') 
 
+const flowMenuRest = addKeyword(EVENTS.ACTION)
+    .addAnswer('Este es el flow menu')
+
+const flowReservar = addKeyword(EVENTS.ACTION)
+    .addAnswer('Este es el flow reservar')
+
+const flowConsultas = addKeyword(EVENTS.ACTION)
+    .addAnswer('Este es el flow consultas')
+
 const flowWelcome = addKeyword(EVENTS.WELCOME)
 .addAnswer("Este es el flujo Welcome", {
     delay: 1000,
@@ -17,7 +31,7 @@ const flowWelcome = addKeyword(EVENTS.WELCOME)
 },
     // Contexto, contextoFront(numero de telefono y nombre)
     async(ctx, ctxFn) => {
-        console.log(ctx.body) // Recoge los mensajes del usario
+        console.log(ctx.body) // Recoge los mensajes del usuario
         //Usar el flujo dinamico con un if
         if (ctx.body.includes("Casas")) {
             await ctxFn.flowDynamic("Escribiste casas")
@@ -28,26 +42,23 @@ const flowWelcome = addKeyword(EVENTS.WELCOME)
 )
 
 const menuFlow = addKeyword("Menu").addAnswer(
-    "Este es el menú, elige opción 1,2,3,4,5 o 0",
+    menu,
     { capture: true },
     async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
-      if (!["1", "2", "3", "4", "5", "0"].includes(ctx.body)) {
+      if (!["1", "2", "3", "0"].includes(ctx.body)) {
         return fallBack(
           "Respuesta no válida, por favor selecciona una de las opciones."
         );
       }
-  
+      
+      //goToFlow("flow..."): Para in entre flujos
       switch (ctx.body) {
         case "1":
-          return await flowDynamic("Esta es la opción 1");
+          return gotoFlow("flowMenuRest");
         case "2":
-          return await flowDynamic("Esta es la opción 2");
+          return gotoFlow("flowReservar");
         case "3":
-          return await flowDynamic("Esta es la opción 3");
-        case "4":
-          return await flowDynamic("Esta es la opción 4");
-        case "5":
-          return await flowDynamic("Esta es la opción 5");
+          return gotoFlow("flowConsultas");
         case "0":
           return await flowDynamic(
             "Saliendo... Puedes volver a acceder a este menú escribiendo '*Menu*'"
@@ -58,7 +69,8 @@ const menuFlow = addKeyword("Menu").addAnswer(
   
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, flowWelcome, menuFlow])
+    const adapterFlow = createFlow([flowPrincipal, flowWelcome, menuFlow, 
+      flowConsultas, flowReservar, flowMenuRest])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
