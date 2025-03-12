776e90977c4dd1da612bc61a596a1be27e55153c
Sebastian Landeo <sebastian.landeo@unmsm.edu.pe>
2025-03-12 13:13:30 -0500

    FlujoConsultas implementado con Gemeni API

--- flujos/flowConsultas.js
+++ flujos/flowConsultas.js
@@ -1,32 +1,33 @@
 const { addKeyword, EVENTS } = require('@bot-whatsapp/bot')
-const connection = require('../mysql') // Importamos la conexión a MySQL
-const util = require('util')
+const chat = require('../gemini') // Importamos la función chat de Gemini
+require('dotenv').config() // Importamos las variables de entorno
+//Texto del prompt
+const path = require('path')
+const fs = require('fs')
+const consultasPath = path.join(__dirname, '..', 'mensajes', 'promptConsultas.txt')
+const promptConsultas = fs.readFileSync(consultasPath, 'utf-8')
 
-// Promisificar la función query
-const query = util.promisify(connection.query).bind(connection)
 
 const flowConsultas = addKeyword(EVENTS.ACTION)
-  .addAnswer('Aquí podrás hacer tus consultas', null, async (ctx, { flowDynamic }) => {
-    try {
-      console.log('Ejecutando consulta SQL...')
-
-      const rows = await query('SELECT * FROM medicos')
-
-      if (rows.length === 0) {
-        await flowDynamic('No hay médicos registrados.')
-        return
+    .addAnswer('Realiza tu consulta', { capture: true }, async (ctx, ctxFn) => {
+        const prompt = promptConsultas
+        const consulta = ctx.body
+        const answer = await chat(prompt, consulta)
+        await ctxFn.flowDynamic(answer)
+    })
+    .addAnswer(
+      "Quieres continuar con otra consulta?`\n1. Sí\n2. No",
+      { capture: true },
+      async (ctx, { fallBack, flowDynamic, gotoFlow}) => {
+        const respuesta = ctx.body
+        if (respuesta === '1' || respuesta.toLowerCase() === 'sí' || respuesta.toLowerCase() === 'si') {
+            return gotoFlow(flowConsultas)
+        } else if (respuesta === '2' || respuesta.toLowerCase() === 'no') {
+            await flowDynamic('Escriba *Menu* para escoger otra opción.')
+        } else {
+            return fallBack('Respuesta no válida, por favor selecciona 1 para Sí o 2 para No.')
+        }
       }
-
-      let respuesta = 'Lista de médicos:\n'
-      rows.forEach(medico => {
-        respuesta += `👨‍⚕️ ${medico.nombre} ${medico.apellido}\n`
-      })
-
-      await flowDynamic(respuesta)
-    } catch (error) {
-      console.error('Error al consultar la base de datos:', error)
-      await flowDynamic('Hubo un error al recuperar los datos. Por favor, inténtalo de nuevo más tarde.')
-    }
-  })
+    )
 
 module.exports = flowConsultas
